5dee2c5c298175812f799620c0d73fed681f522f
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-08-26 14:59:53 +0300

    using the event native object

---
+++ 02 - Basics/basics-05-native-event-object/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    }
+  },
+});
+
+app.mount('#events');
