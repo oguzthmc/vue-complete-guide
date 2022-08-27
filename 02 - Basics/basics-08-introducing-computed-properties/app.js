e02f13bda4b93bca95806bee8a80d9319477ed5f
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-08-27 14:21:19 +0300

    methods used for data binding: how it works

---
+++ 02 - Basics/basics-08-introducing-computed-properties/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    outputFullname() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Tohumcu';
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+    resetInput() {
+      this.name = '';
+    }
+  },
+});
+
+app.mount('#events');
