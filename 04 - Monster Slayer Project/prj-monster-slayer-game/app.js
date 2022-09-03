d4ef31d7897de56c83409c35671a27e7ff22eba9
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-09-04 02:05:05 +0300

    adding a special attack

--- 04 - Monster Slayer Project/prj-monster-slayer-game/app.js
+++ 04 - Monster Slayer Project/prj-monster-slayer-game/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0
     };
   },
   computed: {
@@ -15,10 +16,14 @@ const app = Vue.createApp({
     },
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
+    },
+    isAvailableSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -26,6 +31,12 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     }
   },
 });
