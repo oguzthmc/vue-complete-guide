b735e6cd15cebd1a830894009bc19350921db541
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-09-07 17:34:16 +0300

    one app vs multiple apps

---
+++ 05 - Behind Scenes/behind-scenes-03-one-app-vs-multiple-apps/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount('#app');
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: 'Pizza'
+    };
+  }
+});
+
+app2.mount('#app2');
