acaaff02ab4aec8a5c307b584e155d8bcb27c9c1
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-09-09 20:31:48 +0300

    adding a component

---
+++ 07 - Better Dev Setup & Workflow Vue CLI/vue-cli-02-adding-component/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
