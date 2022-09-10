095e157b65bfe43f7cff401a4e5ea741564ed2dd
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2022-09-10 19:54:50 +0300

    prop behavior & changing props

--- 08 - Component Communication/cmp-comm-02-introducing-props/src/components/FriendContact.vue
+++ 08 - Component Communication/cmp-comm-02-introducing-props/src/components/FriendContact.vue
@@ -1,6 +1,9 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : ''}}</h2>
+    <button @click="toggleFavorite">
+      Toggle Favorite
+    </button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
     </button>
@@ -19,7 +22,7 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress"],
+  props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
   data() {
     return {
       detailsAreVisible: false,
@@ -29,12 +32,20 @@ export default {
         phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
+      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
+    toggleFavorite() {
+      if (this.friendIsFavorite === '1') {
+        this.friendIsFavorite = '0';
+      } else {
+        this.friendIsFavorite = '1';
+      }
+    }
   },
 };
 </script>
